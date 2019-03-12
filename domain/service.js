41f1ca8f91e83cf4a53770006b9b1be4e2bbc129
Johan Dewe <johan@dewe.net>
2019-03-12 21:42:33 +0100

    Add tests for cake service

---
+++ domain/service.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function bake() {
+    
+}
+
+function addFrosting(cake) {
+    
+}
+
+function makeColor(cake, color) {
+    
+}
+
+module.exports = {
+    bake,
+    addFrosting,
+    makeColor
+}
+
---
+++ test/service.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const assert = require('assert');
+const service = require('../domain/service.js')
+
+describe('cake domain service', () => {
+
+    it('can bake cake', () => {
+        let events = service.bake('my-cake')
+
+        assert.deepStrictEqual(events, [
+            {
+                event: 'cake created',
+                data: {
+                    id: 'my-cake'
+                }
+            }
+        ])
+    })
+
+    it('can add frosting', () => {
+        let cake = { id: 'fake-cake' }
+        let events = service.addFrosting(cake)
+
+        assert.deepStrictEqual(events, [
+            {
+                event: 'frosting added',
+                data: {
+                    id: 'my-cake',
+                    frosting: 'cream',
+                    color: 'white'
+                }
+            }
+        ])
+    })
+
+    it('can add color to frosting', () => {
+        let cake = { id: 'fake-cake', frosting: 'cream', color: 'white' }
+
+        let events = service.makeColor(cake, 'red')
+
+        assert.deepStrictEqual(events, [
+            {
+                event: 'added color',
+                data: {
+                    id: 'my-cake',
+                    color: 'red'
+                }
+            }
+        ])
+    })
+
+    it('can not re-color frosting', () => {
+        let cake = { id: 'fake-cake', frosting: 'cream', color: 'red' }
+
+        assert.throws(() => { service.makeColor(cake, 'green') }, 'ColorError')
+    })
+
+})
