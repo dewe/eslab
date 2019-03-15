ec3e15079cd86348b432559ac4924f9eaf29963b
Johan Dewe <johan@dewe.net>
2019-03-15 13:31:40 +0100

    Rewrite aggregate tests

--- test/testAggregate.js
+++ test/testAggregate.js
@@ -6,24 +6,24 @@ describe('cake aggregate', () => {
 
     describe('applying events', () => {
         let event = { eventType: 'cake created', data: { id: 'my-cake' } }
-        let reducer = aggregate.reducer
+        let event2 = { eventType: 'frosting added', data: { frosting: 'cream' } }
 
         it('applies event data', () => {
-            cake = reducer({}, event)
+            cake = aggregate.apply([event])
             assert(cake.id == 'my-cake')
         })
 
-        it('returns a copy', () => {
-            initial = {}
-            cake = reducer(initial, event)
-            assert(cake != initial)
+        it('returns a new copy', () => {
+            cake1 = aggregate.apply([event])
+            cake2 = aggregate.apply([event])
+            assert(cake1 != cake2)
         })
 
-        it('can be used for reducing multiple events', () => {
-            let events = [event, event]
-            cake = events.reduce(reducer, {})
+        it('can hydrate from multiple events', () => {
+            cake = aggregate.apply([event, event2])
             assert(cake.id)
+            assert(cake.frosting)
         })
-
     })
+
 })
